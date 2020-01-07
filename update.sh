dea1c9b33c0ec61511f163b679daf4d1f0c457d6
Iosmanthus Teng <myosmanthustree@gmail.com>
2020-01-07 09:29:37 +0800

    Init commit
    Signed-off-by: Iosmanthus Teng <myosmanthustree@gmail.com>

---
+++ update.sh
@@ -0,0 +1,24 @@
+git submodule update --remote
+
+if [ -z "$1" ]; then
+  echo "Missing set name"
+  exit 1
+fi
+
+sudo systemctl stop ipset.service
+sudo iptables -t nat -F
+sudo ipset destroy $1
+sudo ipset create $1 hash:net
+
+for ip_range in `cat ./private.txt`; do
+  echo "Adding $ip_range to $1"
+  sudo ipset --add $1 $ip_range
+done
+
+for ip_range in `cat ./china_ip_list/china_ip_list.txt`; do
+  echo "Adding $ip_range to $1"
+  sudo ipset --add $1 $ip_range
+done
+
+sudo sh -c 'ipset save > /etc/resolv.conf'
+sudo systemctl restart iptables.service
